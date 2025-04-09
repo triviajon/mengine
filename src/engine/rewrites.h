45a5339502456bd39061151fdfabed9a3d2cd7bf
Jon Rosario <jonros@JonPC.>
2025-04-09 13:13:54 -0400

    don't let exprs get big

---
+++ src/engine/rewrites.h
@@ -0,0 +1,24 @@
+#ifndef REWRITES_H
+#define REWRITES_H
+
+#include "axiom.h"
+#include "beta_reduction.h"
+#include "context.h"
+#include "logic.h"
+#include "proof_state.h"
+#include "rewrite_proof.h"
+#include "unify.h"
+#include "rewrites.h"
+
+RewriteProof *_rewrite(Context *goal_context, Expression *expr, Expression *lemma);
+RewriteProof *_rewrites(Context *goal_context, Expression *expr, int n, va_list lemmas);
+void clear_rewrite_proofs(Expression *expr);
+
+RewriteProof *rewrite_head(Context *goal_context, Expression *expr, Expression *lemma);
+RewriteProof *rewrites_head(Context *goal_context, Expression *expr, int n, va_list lemmas);
+RewriteProof *rewrite(Context *goal_context, Expression *expr, Expression *lemma);
+RewriteProof *rewrites(Context *goal_context, Expression *expr, int n, va_list lemmas);
+RewrittenGoal *rewrite_transform(Expression *goal, Expression *rewrite_lemma);
+RewrittenGoal *rewrites_transform(Expression *goal, int n, ...);
+
+#endif
