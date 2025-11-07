eb328e82392c2c2132b9423337709cf696550f7e
Jon Rosario <jon.rosario@datadoghq.com>
2025-11-07 17:09:03 -0500

    Add doc-strings to beta_reduction.h

--- src/kernel/beta_reduction.h
+++ src/kernel/beta_reduction.h
@@ -4,14 +4,19 @@
 #include "expression.h"
 #include "subst.h"
 
+// Returns true if app_func and app_arg form a reducible expression.
 bool forms_redex(Expression *app_func, Expression *app_arg);
 
+// Reduces a reducible expression to a normal form.
 Expression *reduce(Expression *app_func, Expression *app_arg);
 
+// Normalizes an expression to a normal form.
 Expression *normalize(Expression *expression);
 
+// Normalizes the type of a hole expression to a normal form.
 void normalize_hole_type(Expression *expression);
 
+// Normalizes an expression to a weak head normal form.
 Expression *weak_head_normalize(Expression *expression);
 
 #endif  // BETA_REDUCTION_H
