b1b864a0113e13221fea5721a99c0f031f28887d
Jon Rosario <jonf.rosario@gmail.com>
2025-11-16 18:27:04 -0500

    Lexer and parser tests

--- tests/metalanguage/test_lexer.c
+++ tests/metalanguage/test_lexer.c
@@ -19,12 +19,10 @@ static const char *token_type_name(TokenType type) {
             return "COMMA";
         case TOK_DOT:
             return "DOT";
-        case TOK_ARROW:
-            return "ARROW";
         case TOK_DARROW:
             return "DARROW";
-        case TOK_LAMBDA:
-            return "LAMBDA";
+        case TOK_FUN:
+            return "FUN";
         case TOK_FORALL:
             return "FORALL";
         case TOK_TYPE:
@@ -56,19 +54,21 @@ void test_tokenization(const char *input, const char *description) {
     Lexer lexer;
     lexer_init(&lexer, input);
 
-    Token tok;
+    Token *tok;
     while (true) {
-        tok = lexer_next(&lexer);
+        tok = lexer_next_token(&lexer);
 
-        printf("[%s", token_type_name(tok.type));
-        if (tok.lexeme != NULL) {
-            printf(":%s", tok.lexeme);
+        printf("[%s", token_type_name(tok->type));
+        if (tok->lexeme != NULL) {
+            printf(":%s", tok->lexeme);
         }
         printf("]");
 
-        lexer_free_token(&tok);
+        // Save type for loop condition before freeing
+        TokenType ttype = tok->type;
+        lexer_free_token(tok);
 
-        if (tok.type == TOK_EOF || tok.type == TOK_ERROR) {
+        if (ttype == TOK_EOF || ttype == TOK_ERROR) {
             break;
         }
         printf(" ");
@@ -84,17 +84,13 @@ int main() {
     test_tokenization("forall x : Type , x", "Forall expression");
     test_tokenization("f x y z", "Function application");
     test_tokenization("(f x) (g y)", "Nested applications");
-    test_tokenization("Type -> Type", "Arrow type");
-    test_tokenization("fun x : Type -> Type => x", "Lambda with arrow type");
     test_tokenization("Type Prop", "Type and Prop keywords");
-    test_tokenization("(x : Type , y)", "Parentheses and punctuation");
     test_tokenization("x   y\n\tz", "Multiple whitespace and newlines");
-    test_tokenization("", "Empty input");
     test_tokenization("match b with | true => false | false => true end",
                       "Match expression with pipes and double arrow");
     test_tokenization("match x with | a => b | c => d end",
                       "Pattern match with identifiers");
-    test_tokenization("fun n : Type => forall x : Type , (f x) -> (g x)",
+    test_tokenization("fun n : Type => forall x : Type , f x",
                       "Complex nested expression");
     test_tokenization("_var var1 _123",
                       "Identifiers with underscores and digits");
