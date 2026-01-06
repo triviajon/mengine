563d28166c32f75fe5968bb3f80844bc35d7fb1f
Jon Rosario <jon.rosario@datadoghq.com>
2026-01-05 23:41:20 -0500

    Refactor tokens into X macros

--- src/common/lexer.h
+++ src/common/lexer.h
@@ -6,69 +6,35 @@
 
 #include "src/common/options.h"
 
+// clang-format off
 typedef enum {
-    // identifiers, punctuation
-    TOK_IDENT,
-    TOK_LPAREN,    // (
-    TOK_RPAREN,    // )
-    TOK_LBRACE,    // {
-    TOK_RBRACE,    // }
-    TOK_COLON,     // :
-    TOK_COLON_EQ,  // :=
-    TOK_COMMA,     // ,
-    TOK_DOT,       // .
+    TOK_IDENT,      // identifiers
 
-    // term keywords
-    TOK_DARROW,  // =>
-    TOK_FUN,     // fun
-    TOK_FORALL,  // forall
-    TOK_TYPE,    // Type
-    TOK_PROP,    // Prop
-    TOK_FIX,     // fix
-    TOK_STRUCT,  // struct
-    TOK_LET,     // let
-    TOK_IN,      // in
-    TOK_MATCH,   // match
-    TOK_WITH,    // with
-    TOK_PIPE,    // |
-    TOK_END,     // end
+    // keywords
+    #define X(lexeme, tok, dbg) tok,
+    #include "src/common/token_keywords.def"
+    #undef X
 
-    // command keywords
-    TOK_AXIOM,       // Axiom
-    TOK_VARIABLE,    // Variable
-    TOK_DEFINITION,  // Definition
-    TOK_THEOREM,     // Theorem
-    TOK_LEMMA,       // Lemma
-    TOK_FIXPOINT,    // Fixpoint
-    TOK_CHECK,       // Check
-    TOK_PRINT,       // Print
-    TOK_SHOW,        // Show
-    TOK_CONTEXT,     // Context
-    TOK_PROOF,       // Proof
-    TOK_GOAL,        // Goal
-    TOK_STATE,       // State
-    TOK_INDUCTIVE,   // Inductive
-
-    // tactic keywords
-    TOK_ADMITTED,     // Admitted
-    TOK_INTRO,        // intro
-    TOK_INTROS,       // intros
-    TOK_APPLY,        // apply
-    TOK_EAPPLY,       // eapply
-    TOK_EXACT,        // exact
-    TOK_REWRITE,      // rewrite
-    TOK_LEFT_ARROW,   // <-
-    TOK_REFLEXIVITY,  // reflexivity
-    TOK_ASSUMPTION,   // assumption
-    TOK_SPLIT,        // split
-    TOK_LEFT,         // left
-    TOK_RIGHT,        // right
-    TOK_EXISTS,       // exists
+    TOK_LPAREN,      // (
+    TOK_RPAREN,      // )
+    TOK_LBRACE,      // {
+    TOK_RBRACE,      // }
+    TOK_COLON,       // :
+    TOK_COLON_EQ,    // :=
+    TOK_COMMA,       // ,
+    TOK_DOT,         // .
+    TOK_DARROW,      // =>
+    TOK_PIPE,        // |
+    TOK_LEFT_ARROW,  // <-
 
     TOK_COMMENT,
     TOK_EOF,
     TOK_ERROR,
+
+    // magic: we can add a sentinel here and use it as a size
+    NUM_TOKEN_TYPES,
 } TokenType;
+// clang-format on
 
 typedef struct {
     TokenType type;
@@ -88,49 +54,11 @@ typedef struct {
     TokenType kind;
 } Keyword;
 
-// clang-format off
 static const Keyword keywords[] = {
-    #define KEYWORD(name, tok) {name, tok},
-        KEYWORD("fun", TOK_FUN)
-        KEYWORD("forall", TOK_FORALL)
-        KEYWORD("Type", TOK_TYPE)
-        KEYWORD("Prop", TOK_PROP)
-        KEYWORD("fix", TOK_FIX)
-        KEYWORD("struct", TOK_STRUCT)
-        KEYWORD("let", TOK_LET)
-        KEYWORD("in", TOK_IN)
-        KEYWORD("match", TOK_MATCH)
-        KEYWORD("with", TOK_WITH)
-        KEYWORD("end", TOK_END)
-        KEYWORD("Axiom", TOK_AXIOM)
-        KEYWORD("Variable", TOK_VARIABLE)
-        KEYWORD("Definition", TOK_DEFINITION)
-        KEYWORD("Theorem", TOK_THEOREM)
-        KEYWORD("Lemma", TOK_LEMMA)
-        KEYWORD("Fixpoint", TOK_FIXPOINT)
-        KEYWORD("Check", TOK_CHECK)
-        KEYWORD("Print", TOK_PRINT)
-        KEYWORD("Show", TOK_SHOW)
-        KEYWORD("Context", TOK_CONTEXT)
-        KEYWORD("Proof", TOK_PROOF)
-        KEYWORD("Goal", TOK_GOAL)
-        KEYWORD("State", TOK_STATE)
-        KEYWORD("Inductive", TOK_INDUCTIVE)
-        KEYWORD("Admitted", TOK_ADMITTED)
-        KEYWORD("intro", TOK_INTRO)
-        KEYWORD("intros", TOK_INTROS)
-        KEYWORD("apply", TOK_APPLY)
-        KEYWORD("eapply", TOK_EAPPLY)
-        KEYWORD("exact", TOK_EXACT)
-        KEYWORD("rewrite", TOK_REWRITE)
-        KEYWORD("reflexivity", TOK_REFLEXIVITY)
-        KEYWORD("assumption", TOK_ASSUMPTION)
-        KEYWORD("split", TOK_SPLIT)
-        KEYWORD("left", TOK_LEFT)
-        KEYWORD("right", TOK_RIGHT)
-        KEYWORD("exists", TOK_EXISTS)
+#define X(lexeme, tok, dbg) {lexeme, tok},
+#include "src/common/token_keywords.def"
+#undef X
 };
-// clang-format on
 
 /* Lexer API */
 
