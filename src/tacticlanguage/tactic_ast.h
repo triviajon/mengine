5fb38e066c08e9726ae3055993da145dc12b547c
Jon Rosario <jonf.rosario@gmail.com>
2026-04-08 18:15:54 -0400

    tactic_ast: define TacticExpr AST for tactic combinators

---
+++ src/tacticlanguage/tactic_ast.h
@@ -0,0 +1,118 @@
+#ifndef TACTIC_AST_H
+#define TACTIC_AST_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "src/tacticlanguage/tactic_parser.h"
+
+typedef enum {
+    TAC_PRIMITIVE,  // wraps an existing Tactic*
+    TAC_SEQ,        // tac1 ; tac2
+    TAC_ORELSE,     // tac1 || tac2
+    TAC_TRY,        // try tac
+    TAC_REPEAT,     // repeat tac
+    TAC_FIRST,      // first [ tac1 | tac2 | ... ]
+    TAC_IDTAC,      // idtac (identity, always succeeds)
+    TAC_FAIL,       // fail (always fails)
+} TacticExprTag;
+
+typedef struct TacticExpr TacticExpr;
+
+typedef struct {
+    Tactic *tactic;
+} PrimitiveTacticExpr;
+
+typedef struct {
+    TacticExpr *left;
+    TacticExpr *right;
+} SeqTacticExpr;
+
+typedef struct {
+    TacticExpr *left;
+    TacticExpr *right;
+} OrelseTacticExpr;
+
+typedef struct {
+    TacticExpr *body;
+} TryTacticExpr;
+
+typedef struct {
+    TacticExpr *body;
+} RepeatTacticExpr;
+
+typedef struct {
+    TacticExpr **alternatives;
+    size_t count;
+} FirstTacticExpr;
+
+struct TacticExpr {
+    TacticExprTag tag;
+    union {
+        PrimitiveTacticExpr primitive;
+        SeqTacticExpr seq;
+        OrelseTacticExpr orelse;
+        TryTacticExpr try_expr;
+        RepeatTacticExpr repeat;
+        FirstTacticExpr first;
+    } as;
+};
+
+static inline TacticExpr *tactic_expr_primitive(Tactic *tactic) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_PRIMITIVE;
+    e->as.primitive.tactic = tactic;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_seq(TacticExpr *left, TacticExpr *right) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_SEQ;
+    e->as.seq.left = left;
+    e->as.seq.right = right;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_orelse(TacticExpr *left, TacticExpr *right) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_ORELSE;
+    e->as.orelse.left = left;
+    e->as.orelse.right = right;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_try(TacticExpr *body) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_TRY;
+    e->as.try_expr.body = body;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_repeat(TacticExpr *body) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_REPEAT;
+    e->as.repeat.body = body;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_first(TacticExpr **alternatives, size_t count) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_FIRST;
+    e->as.first.alternatives = alternatives;
+    e->as.first.count = count;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_idtac(void) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_IDTAC;
+    return e;
+}
+
+static inline TacticExpr *tactic_expr_fail(void) {
+    TacticExpr *e = malloc(sizeof(TacticExpr));
+    e->tag = TAC_FAIL;
+    return e;
+}
+
+#endif  // TACTIC_AST_H
